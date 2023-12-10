8c19634a67c3d732c12dc5fb96fad9c12ea4c2b3
Cyberboysatyam <139473783+Cyberboysatyam@users.noreply.github.com>
2023-12-10 12:22:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyAIUbSZySfXpXGAaBhBJuopZMWGANSlnjg",
+    authDomain: "newvirus-b0191.firebaseapp.com",
+    databaseURL: "https://newvirus-b0191-default-rtdb.firebaseio.com",
+    projectId: "newvirus-b0191",
+    storageBucket: "newvirus-b0191.appspot.com",
+    messagingSenderId: "469051696298",
+    appId: "1:469051696298:android:4d095bdcc7a1ab3b365cd3"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
